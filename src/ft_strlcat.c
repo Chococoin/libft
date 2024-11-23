3ef48c2dffaa53a89b7122a4054fa10f373c7000
German G. Lugo Munoz <glugo-mu@42firenze.it>
2024-11-23 12:45:47 +0100

    Fix ft.

--- src/ft_strlcat.c
+++ src/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: glugo-mu <glugo-mu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/23 10:09:34 by glugo-mu          #+#    #+#             */
-/*   Updated: 2024/11/23 10:27:11 by glugo-mu         ###   ########.fr       */
+/*   Updated: 2024/11/23 12:44:43 by glugo-mu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,14 @@ int	ft_strlcat(char *dest, const char *src, int size)
 	int	src_len;
 	int	i;
 
+	dest_len = 0;
+	src_len = 0;
 	while (dest[dest_len] != '\0' && dest_len < size)
-	{
 		dest_len++;
-	}
 	while (src[src_len] != '\0')
-	{
 		src_len++;
-	}
 	if (size <= dest_len)
-	{
 		return (size + src_len);
-	}
 	i = 0;
 	while (src[i] != '\0' && dest_len + i < size - 1)
 	{
